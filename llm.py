4d11eb9b5e89c6e2f5ab5fec4a68f6002817ca28
SilentStormCoder <tjx2001x3@gmail.com>
2025-05-21 11:37:54 -0700

    added llm stuff

--- llm.py
+++ llm.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain.prompts import ChatPromptTemplate
-from langchain.schema.runnable import RunnableParallel, RunnableLambda
+from langchain.schema.runnable import RunnableBranch, RunnableLambda
 from langchain.schema.output_parser import StrOutputParser
 import openai
 import logging
@@ -14,70 +14,95 @@ import logging
 
 # model = gemini_model
 
-# classifier_prompt_template = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", """You are an assistant that will classify the types of prompts by pilots that are sent to you. 
-#                         """),
-#         ("human", {"""You must assign the following prompt exactly one number from 0-2. 
-#                       Assign a 1 if the request is for radio command translation.
-#                       Assign a 2 if the request is for flight manual assistance.
-#                       Assign a 0 if the request is for anything else: 
-#                       {prompt}"""})
+classifier_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", """You are an assistant that will classify the types of prompts by pilots that are sent to you. 
+                        """),
+        ("human", {"""You must assign the following prompt exactly one number from 0-2. 
+                      Assign a 1 if the request is for radio command translation.
+                      Assign a 2 if the request is for flight manual assistance.
+                      Assign a 0 if the request is for anything else: 
+                      {prompt}"""})
         
-#     ]
-# )
+    ]
+)
 
 
-# radio_command_translation_template = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", "You are an assistant that will decode the radio command sent by the user and explain in detail what it means."),
-#         ("human", "{prompt}")
-#     ]
-# )
+radio_command_translation_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an assistant that will decode the radio command sent by the user and explain in detail what it means."),
+        ("human", "{prompt}")
+    ]
+)
 
 
-# radio_command_translation_better = RunnableLambda(
+radio_command_translation_better = RunnableLambda(
 
-# )
+)
 
-# flight_manual_assistance_template = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", "You are an assistant that will assist pilots with their questions related to flight manuals. Answer their questions in detail."),
-#         ("human", "{prompt}")
-#     ]
-# )
+flight_manual_assistance_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an assistant that will assist pilots with their questions related to flight manuals. Answer their questions in detail."),
+        ("human", "{prompt}")
+    ]
+)
 
-# general_assistance_template = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", "You are an assistant that will classify the types of prompts by pilots that are sent to you."),
-#         ("human", "{prompt}")
-#     ]
-# )
+general_assistance_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an assistant that will classify the types of prompts by pilots that are sent to you."),
+        ("human", "{prompt}")
+    ]
+)
 
-# task_branches = RunnableBranch(
-#     (
-#         lambda x: "1" in x,
-#         radio_command_translation_template | model | StrOutputParser
-#     ),
-#     (
-#         lambda x: "2" in x,
-#         flight_manual_assistance_template | model | StrOutputParser
-#     ),
-#     general_assistance_template | model | StrOutputParser
-# )
+task_branches = RunnableBranch(
+    (
+        lambda x: "1" in x,
+        radio_command_translation_template | model | StrOutputParser
+    ),
+    (
+        lambda x: "2" in x,
+        flight_manual_assistance_template | model | StrOutputParser
+    ),
+    general_assistance_template | model | StrOutputParser
+)
 
-# classifier_chain = classifier_prompt_template | model | StrOutputParser
+classifier_chain = classifier_prompt_template | model | StrOutputParser
 
-# request_chain = classifier_chain | task_branches
+request_chain = classifier_chain | task_branches
 model = None
 def get_model():
     if model is None:
         model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
     return model
 
+chain = None
+def get_chain():
+    if chain is None:
+        
+        task_branches = RunnableBranch(
+            (
+                lambda x: "1" in x,
+                radio_command_translation_template | model | StrOutputParser
+            ),
+            (
+                lambda x: "2" in x,
+                flight_manual_assistance_template | model | StrOutputParser
+            ),
+            general_assistance_template | model | StrOutputParser
+        )
+
+        classifier_chain = classifier_prompt_template | model | StrOutputParser
+
+        chain = classifier_chain | task_branches
+
+    return chain
+
 def LLMRequestHandler(prompt):
-    #response = request_chain.invoke({"prompt": prompt})
-    pass
+    logging.info(f"Request handling started. Prompt: {prompt}")
+    llm_chain = get_chain()
+    response = request_chain.invoke({"prompt": prompt})
+    logging.info(f"Request handled. Result: {response}")
+    return response
 def speechToText(file):
     logging.info("API request is being sent")
     try:
