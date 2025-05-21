19a354bd29d427d6dd2793fee1cefdc0c259102c
SilentStormCoder <tjx2001x3@gmail.com>
2025-05-20 23:05:50 -0700

    added templates for llm and fixed speechtotext

--- llm.py
+++ llm.py
@@ -1,19 +1,88 @@
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
-import whisper
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableParallel, RunnableLambda
+from langchain.schema.output_parser import StrOutputParser
+import openai
 
 # Referenced this video in creating this section: https://www.youtube.com/watch?v=yF9kGESAi3M
 
 GPT_model = ChatOpenAI(model="gpt-4o")
 gemini_model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
-speech_to_text_model = whisper.load_model("base")
+
+model = gemini_model
+
+classifier_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", """You are an assistant that will classify the types of prompts by pilots that are sent to you. 
+                        """),
+        ("human", {"""You must assign the following prompt exactly one number from 0-2. 
+                      Assign a 1 if the request is for radio command translation.
+                      Assign a 2 if the request is for flight manual assistance.
+                      Assign a 0 if the request is for anything else: 
+                      {prompt}"""})
+        
+    ]
+)
+
+
+radio_command_translation_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", """You are an assistant that will classify the types of prompts by pilots that are sent to you. 
+                      You must assign each prompt a number from 0-2. 
+                      Assign a 1 if the request is for radio command translation.
+                      Assign a 2 if the request is for flight manual assistance.
+                      Assign a 0 if the request is for anything else.  """),
+        ("human", "{prompt}")
+    ]
+)
+
+flight_manual_assistance_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", """You are an assistant that will classify the types of prompts by pilots that are sent to you. 
+                      You must assign each prompt a number from 0-2. 
+                      Assign a 1 if the request is for radio command translation.
+                      Assign a 2 if the request is for flight manual assistance.
+                      Assign a 0 if the request is for anything else.  """),
+        ("human", "{prompt}")
+    ]
+)
+
+general_assistance_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", """You are an assistant that will classify the types of prompts by pilots that are sent to you. 
+                      You must assign each prompt a number from 0-2. 
+                      Assign a 1 if the request is for radio command translation.
+                      Assign a 2 if the request is for flight manual assistance.
+                      Assign a 0 if the request is for anything else.  """),
+        ("human", "{prompt}")
+    ]
+)
+
+branches = RunnableBranch(
+    (
+        lambda x: "1" in x,
+        radio_command_translation_template | model | StrOutputParser
+    ),
+    (
+        lambda x: "2" in x,
+        flight_manual_assistance_template | model | StrOutputParser
+    ),
+    general_assistance_template | model | StrOutputParser
+)
+
+
 
 def LLMRequestHandler(message):
     pass
 
 def speechToText(file):
-    return speech_to_text_model.transcribe(file)
+    text = openai.Audio.transcribe(
+                model="whisper-1",
+                file=file
+            )
+    return text
 
 def radioCommandTranslation(message):
     system_message = [
